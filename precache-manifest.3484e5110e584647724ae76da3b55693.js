d27b46a3eb6fc0c6104ec28a02d7c24c90b0e704
Rdyx <tiranico.pro@gmail.com>
2020-10-26 11:57:23 +0100

    Push build

---
+++ precache-manifest.3484e5110e584647724ae76da3b55693.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5301f68d95efd5369dc9d4e942a9c23a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2ba110dabc11958fd456",
+    "url": "./static/css/2.c72968ad.chunk.css"
+  },
+  {
+    "revision": "6b0ee6a44d690d38ffb2",
+    "url": "./static/css/main.f6e5e572.chunk.css"
+  },
+  {
+    "revision": "2ba110dabc11958fd456",
+    "url": "./static/js/2.3ad14d4f.chunk.js"
+  },
+  {
+    "revision": "23448078f552659a2bd406b0b52fe386",
+    "url": "./static/js/2.3ad14d4f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b0ee6a44d690d38ffb2",
+    "url": "./static/js/main.3448a3bc.chunk.js"
+  },
+  {
+    "revision": "8185ab8f8a8eb864858e",
+    "url": "./static/js/runtime-main.9a7956a2.js"
+  },
+  {
+    "revision": "7ef87c93d54fb9a2c78dfdd7d9e7ce80",
+    "url": "./static/media/arrow.7ef87c93.svg"
+  },
+  {
+    "revision": "60f532b73b8720aba4a9f3fb2d39c56d",
+    "url": "./static/media/facebook.60f532b7.svg"
+  },
+  {
+    "revision": "0255bf2cd6e42c2fdbe2e189e418929f",
+    "url": "./static/media/github-icon.0255bf2c.svg"
+  },
+  {
+    "revision": "f3e24db27736030c70accbf2f2c852e4",
+    "url": "./static/media/paypal.f3e24db2.svg"
+  },
+  {
+    "revision": "5df9c3c7e73bb6e3522eedc439865f9b",
+    "url": "./static/media/reddit.5df9c3c7.svg"
+  }
+]);
